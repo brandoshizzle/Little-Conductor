0df02d049b36c7b1172e46263a65968acbd482c5
brandoshizzle <cathcart.brandon@gmail.com>
2020-07-04 15:10:16 -0600

    Updates

---
+++ precache-manifest.d29ce98344e2b25de9b4b6ce83bd9e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12911067e00a6217e466",
+    "url": "/Little-Conductor/static/css/main.217e617c.chunk.css"
+  },
+  {
+    "revision": "12911067e00a6217e466",
+    "url": "/Little-Conductor/static/js/main.e5396ab3.chunk.js"
+  },
+  {
+    "revision": "bcee38596231b09597bb",
+    "url": "/Little-Conductor/static/js/runtime~main.018d774d.js"
+  },
+  {
+    "revision": "7d7ff06d74b7a416111b",
+    "url": "/Little-Conductor/static/js/2.bd332baa.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Little-Conductor/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "9b25e4847c9aa7cb5c10f6ffd34ed9d1",
+    "url": "/Little-Conductor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Little-Conductor/precache-manifest.9c00876587d47b406a28a85582bf2e1e.js"
+  "/Little-Conductor/precache-manifest.d29ce98344e2b25de9b4b6ce83bd9e83.js"
 );
 
 workbox.clientsClaim();
